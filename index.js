5170192c987dc25445f35191586591a7c2a18b1c
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-08-09 18:00:33 +0530

    UC7: Classes and objects

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 let con = require('./constants')
+var totalEmpHrs = 0;
+var totalWorkDays = 0;
 
-console.log("Welcome to Employee Wage Program");
+class Employee{
+    constructor(){
+        //Welcome message
+        this.message();
+    }
 
-{
-    var totalEmpHrs = 0;
-    var totalWorkDays = 0;
-    var wageArray = [];
+    message(){
+        console.log("Welcome to Employee Wage Program");
+        
+        this.calculateWage();
+    }
+
+    calculateWage(){
+        while(totalEmpHrs < con.maxHrsMonth && totalWorkDays < con.maxDaysMonth){
+            totalWorkDays++;
+            var emp_case = Math.floor(Math.random()*10)%3;
+            totalEmpHrs += this.getHours(emp_case);
+        }
     
-    while(totalEmpHrs < con.maxHrsMonth && totalWorkDays < con.maxDaysMonth){
-        totalWorkDays++;
-        var emp_case = Math.floor(Math.random()*10)%3;
-        totalEmpHrs += getHours(emp_case);
+        var emp_wage = totalEmpHrs * con.wageperhour;
+        console.log("Day: "+ totalWorkDays+ " EmpHr: " + totalEmpHrs+ " EmpWage: "+emp_wage);
     }
 
-    console.log("Wages for each day: "+wageArray);
-    console.log("Total Hours:"+totalEmpHrs+" Total Workdays:"+totalWorkDays);
+    getHours(emp_case){
 
-    function getHours(emp_case){
-
-    switch(emp_case){
-        case 1: //fulltime
-           empHours = con.fulltime;
-           break;
-
-        case 0: //parttime
-            empHours = con.parttime;
-            break;
-
-        default:
-            empHours = 0;
-            break;
+        switch(emp_case){
+            case 1: //fulltime
+               this.empHours = con.fulltime;
+               break;
+    
+            case 0: //parttime
+                this.empHours = con.parttime;
+                break;
+    
+            default:
+                this.empHours = 0;
+                break;
+        }
+        return this.empHours;
     }
-    dayWage = empHours * con.wageperhour;
-    wageArray.push(dayWage);
-    return empHours;
 }
 
-}
+let newEmp = new Employee();
