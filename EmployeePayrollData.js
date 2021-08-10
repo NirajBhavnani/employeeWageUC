4b269f73a254fc677e411f29aca81c3e4f964053
Niraj Bhavnani <niraj.bhavnani@morningstar.com>
2021-08-10 23:08:52 +0530

    UC13: Classes and extends/Inheritance

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -26,10 +26,42 @@ const EmployeePayrollData = class{
     }
 };
 
-const Emp = new EmployeePayrollData(1, 'Niraj', '100000');
+const Emp = new EmployeePayrollData(1, 'Aditya', '100000');
 
-Emp.setEmpName = "Niraj Bhavnani";
+Emp.setEmpName = "Aditya Uphade";
 Emp.setEmpSalary = 150000;
 
 console.log("Employee Details: ");
 console.log(`Name => ${Emp.empName}, ID => ${Emp.empId}, Salary => ${Emp.empSalary}`);
+
+console.log("****************** EXTENDED EMPLOYEE DATA ******************");
+const NewEmployeePayrollData = class extends EmployeePayrollData{
+    constructor(...params) {
+        super(...params.slice(0,3));//For the first 3 params
+        this.gender = params[3];
+        this.joinDate = params[4];
+    }
+
+    get empGender(){
+        return this.gender;
+    }
+
+    get empJoinDate(){
+        return this.joinDate;
+    }
+
+    set setEmpGender(gender){
+        this.gender = gender;
+    }
+
+    set setEmpJoinDate(jDate){
+        this.joinDate = jDate;
+    }
+};
+
+const Emp2 = new NewEmployeePayrollData(2, 'Darshan', 150000, 'Male', '1 August 2021');
+
+Emp2.setEmpJoinDate = '2 August 2021';
+
+console.log("New Employee Details: ");
+console.log(`Name => ${Emp2.empName}, ID => ${Emp2.empId}, Salary => ${Emp2.empSalary}`);
